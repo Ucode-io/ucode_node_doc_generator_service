562b15a7449997f91b151750cba192ee09c92df8
s.erkinboyev <samandargobackend@gmail.com>
2026-05-23 10:52:09 +0500

    restart

--- config/constants.js
+++ config/constants.js
@@ -2,7 +2,7 @@ const convertapi_secrets = [
    "FIfurq7JjengpchT899ytEaHdY78q4nv"
 ];
 
-/// For backward compatibility
+// For backward compatibility
 const convertapi_secret = convertapi_secrets[0];
 
 module.exports = {
